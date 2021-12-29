7121e0f3048f35f5459841c25f5739a05303963e
Season <65497908+Season111@users.noreply.github.com>
2021-12-29 13:19:28 +0800

    Add files via upload

---
+++ LZU-Auto-Course-Evaluation-Feedback.py
@@ -0,0 +1,186 @@
+# -*- coding:utf-8 -*-
+# by 'hollowman6' from Lanzhou University(兰州大学)
+
+'''
+警告：
+仅供测试使用，不可用于任何非法用途！
+对于使用本代码所造成的一切不良后果，本人将不负任何责任！
+
+Warning:
+For TESTING ONLY, not for any ILLEGAL USE!
+I will not be responsible for any adverse consequences caused by using this code.
+
+'''
+
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome()
+
+print("请在打开的浏览器界面中登录，之后会自动开始评教！")
+
+driver.get("http://my.lzu.edu.cn:8080/login?service=http://my.lzu.edu.cn")
+WebDriverWait(driver, 5)
+
+NetworkErr = False
+while True:
+    if driver.find_element_by_xpath('/html').get_attribute('dir') == "ltr":
+        if not NetworkErr:
+            print("Network error, please check your network!")
+            NetworkErr = True
+        driver.get(
+            "http://my.lzu.edu.cn:8080/login?service=http://my.lzu.edu.cn")
+        time.sleep(3)
+    else:
+        NetworkErr = False
+        break
+
+while True:
+    time.sleep(3)
+    if "http://my.lzu.edu.cn/main" in driver.current_url:
+        break
+
+driver.get("http://qa.lzu.edu.cn:8081/loginSSO2")
+WebDriverWait(driver, 5)
+NetworkErr = False
+while True:
+    if driver.find_element_by_xpath('/html').get_attribute('dir') == "ltr":
+        if not NetworkErr:
+            print("Network error, please check your network!")
+            NetworkErr = True
+        driver.get(
+            "http://qa.lzu.edu.cn:8081/loginSSO2")
+        time.sleep(3)
+    else:
+        NetworkErr = False
+        break
+
+js = '''return document.querySelector('#task-list').getElementsByTagName('li').length'''
+taskList = driver.execute_script(js)
+
+for i in range(1, taskList+1):
+    js = '''return document.querySelector('#task-list>li:nth-child(''' + \
+        str(i)+''')').className'''
+    currentClass = driver.execute_script(js)
+    if currentClass == "over":
+        continue
+    js = '''document.querySelector('#task-list>li:nth-child(''' + \
+        str(i)+''')').click()'''
+    driver.execute_script(js)
+    time.sleep(3)
+
+    js = '''return document.querySelector("#tipDlg > div.modal-footer > button").disabled'''
+    while driver.execute_script(js):
+        time.sleep(3)
+    
+    js = '''document.querySelector("#tipDlg > div.modal-footer > button").click()'''
+    driver.execute_script(js)
+    time.sleep(3)
+
+    js = '''return document.querySelector('#pjkc').rows.length'''
+    rows = driver.execute_script(js)
+
+    for j in range(1, rows+1):
+        js = '''document.querySelector("#pjkc > tr:nth-child(''' + \
+            str(j) + ''') > td:nth-child(3) > div > a").click()'''
+        driver.execute_script(js)
+        time.sleep(3)
+
+        js = '''return document.querySelector("#sample_2>tbody").getElementsByTagName('tr').length'''
+        teachers = driver.execute_script(js)
+
+        if teachers > 3:
+            js = '''document.querySelector("#kfxpjDlg > div.modal-header > button").click()'''
+            driver.execute_script(js)
+            time.sleep(3)
+
+            if driver.execute_script('''return document.querySelector("#pjkc > tr:nth-child(''' + str(j) + ''') > td:nth-child(6)").getElementsByTagName('div').length == 0'''):
+                pass
+            else:
+                js = '''document.querySelector("#pjkc > tr:nth-child(''' + str(
+                    j) + ''') > td:nth-child(6) > div > a").click()'''
+                driver.execute_script(js)
+                time.sleep(3)
+
+                js = '''return document.getElementsByClassName("controls").length'''
+                input = driver.execute_script(js)
+                time.sleep(3)
+
+                for l in range(3, input-3):
+                    js = '''document.getElementsByClassName("controls")['''+str(
+                        l)+'''].getElementsByTagName('label')[0].click()'''
+                    driver.execute_script(js)
+
+                js = '''
+                    document.getElementsByClassName("controls")['''+str(input-3)+'''].getElementsByTagName('label')[1].click();
+                    document.getElementsByClassName("controls")['''+str(input-2)+'''].getElementsByTagName('textarea')[0].value = "好";
+                    document.getElementsByClassName("controls")['''+str(input-1)+'''].getElementsByTagName('textarea')[0].value = "好";
+                    '''
+                driver.execute_script(js)
+                time.sleep(3)
+
+                js = '''document.querySelector("#pjsubmit").click()'''
+                driver.execute_script(js)
+                time.sleep(3)
+
+                js = '''document.querySelector("#finishDlg > div.modal-footer > button").click()'''
+                driver.execute_script(js)
+                time.sleep(3)
+
+            js = '''document.querySelector("#pjkc > tr:nth-child(''' + str(
+                j) + ''') > td:nth-child(3) > div > a").click()'''
+            driver.execute_script(js)
+            time.sleep(3)
+
+            js = '''return document.querySelector("#sample_2>tbody").getElementsByTagName('tr').length'''
+            teachers = driver.execute_script(js)
+            time.sleep(1)
+
+        for k in range(1, teachers+1):
+            time.sleep(0.5)
+            js = '''return document.querySelector("#sample_2 > tbody > tr:nth-child('''+str(
+                k)+''') > td:nth-child(3) > div > a").className'''
+            if (driver.execute_script(js) == "badge badge-success"):
+                continue
+            js = '''document.querySelector("#sample_2 > tbody > tr:nth-child('''+str(
+                k)+''') > td:nth-child(3) > div > a").click()'''
+            driver.execute_script(js)
+            time.sleep(3)
+
+            js = '''return document.getElementsByClassName("controls").length'''
+            input = driver.execute_script(js)
+            time.sleep(3)
+
+            for l in range(3, input-3):
+                js = '''document.getElementsByClassName("controls")['''+str(
+                    l)+'''].getElementsByTagName('label')[0].click()'''
+                driver.execute_script(js)
+
+            js = '''
+                document.getElementsByClassName("controls")['''+str(input-3)+'''].getElementsByTagName('label')[1].click();
+                document.getElementsByClassName("controls")['''+str(input-2)+'''].getElementsByTagName('textarea')[0].value = "好";
+                document.getElementsByClassName("controls")['''+str(input-1)+'''].getElementsByTagName('textarea')[0].value = "好";
+                '''
+            driver.execute_script(js)
+            time.sleep(3)
+
+            js = '''document.querySelector("#pjsubmit").click()'''
+            driver.execute_script(js)
+            time.sleep(3)
+
+            js = '''document.querySelector("#finishDlg > div.modal-footer > button").click()'''
+            driver.execute_script(js)
+            time.sleep(3)
+
+        js = '''document.querySelector("#kfxpjDlg > div.modal-header > button").click()'''
+        driver.execute_script(js)
+        time.sleep(3)
+
+    js = '''document.querySelector("#titlelist > div.actions > div > a").click()'''
+    driver.execute_script(js)
+    time.sleep(3)
+
+driver.close()
+
+print("评教已经结束！请检查是否已经全部评教完成，如果还没有可以多运行几遍本程序。")
--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# LZU_Automatic_evaluation
+# 兰州大学自动评教
+
+[![last-commit](https://img.shields.io/github/last-commit/HollowMan6/LZU-Auto-Course-Evaluation-Feedback)](../../graphs/commit-activity)
+![Python package](https://github.com/HollowMan6/LZU-Auto-Course-Evaluation-Feedback/workflows/Python%20package/badge.svg)
+
+[![Followers](https://img.shields.io/github/followers/HollowMan6?style=social)](https://github.com/HollowMan6?tab=followers)
+[![watchers](https://img.shields.io/github/watchers/HollowMan6/LZU-Auto-Course-Evaluation-Feedback?style=social)](../../watchers)
+[![stars](https://img.shields.io/github/stars/HollowMan6/LZU-Auto-Course-Evaluation-Feedback?style=social)](../../stargazers)
+[![forks](https://img.shields.io/github/forks/HollowMan6/LZU-Auto-Course-Evaluation-Feedback?style=social)](../../network/members)
+
+[![Open Source Love](https://img.shields.io/badge/-%E2%9D%A4%20Open%20Source-Green?style=flat-square&logo=Github&logoColor=white&link=https://hollowman6.github.io/fund.html)](https://hollowman6.github.io/fund.html)
+[![GPL Licence](https://img.shields.io/badge/license-GPL-blue)](https://opensource.org/licenses/GPL-3.0/)
+[![Repo-Size](https://img.shields.io/github/repo-size/HollowMan6/LZU-Auto-Course-Evaluation-Feedback.svg)](../../archive/master.zip)
+
+[![Total alerts](https://img.shields.io/lgtm/alerts/g/HollowMan6/LZU-Auto-Course-Evaluation-Feedback.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/HollowMan6/LZU-Auto-Course-Evaluation-Feedback/alerts/)
+[![Language grade: Python](https://img.shields.io/lgtm/grade/python/g/HollowMan6/LZU-Auto-Course-Evaluation-Feedback.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/HollowMan6/LZU-Auto-Course-Evaluation-Feedback/context:python)
+
+(English version is down below)
+
+[Python库依赖](../../network/dependencies)
+
+此程序使用Python的selenium库，使用前请确保电脑上已经安装了Google Chrome浏览器，并且在程序执行目录下放置了[Chrome Driver](https://chromedriver.chromium.org)
+
+此程序适用于 `http://qa.lzu.edu.cn:8081` 页面的评教。
+
+## 使用方法
+
+只要在程序运行开始的页面输入你的账号和密码，并点击登录，程序将会自动为你评教。
+
+![](login.png)
+
+程序会自动为每个老师给予最高的评价，并在需要填写文字处填写上“好”。
+
+如果该课程老师数量大于3个，程序会首先进行课程的评教，再进行老师的评教。
+
+**注：**：如果遇到有的老师被跳过的问题，可能是由网络状况引起的，建议再次运行一下。
+
+## 链接
+
+* [兰州大学自动评教 脚本](LZU-Auto-Course-Evaluation-Feedback.py)
+
+测试于2021年11月12日, 2021-2022学年第一学期期中评教。
+
+**警告**：
+
+***仅供测试使用，不可用于任何非法用途！***
+
+***对于使用本代码所造成的一切不良后果，本人将不负任何责任！***
+
+# LZU Auto Course Evaluation Feedback
+
+[Python library dependency](../../network/dependencies)
+
+Using selenium to realize the function. Before using, make sure that Google Chrome browser is installed on the computer, and [Chrome Driver](https://chromedriver.chromium.org) is placed in the programme execution directory.
+
+The programme is suitable for course evaluation at `http://qa.lzu.edu.cn:8081`.
+
+## Usage
+
+As long as you enter your account and password on the start page of the program, and click login, the program will automatically do the course evaluation job.
+
+![](login.png)
+
+The program will automatically give each teacher the highest feedback, and fill "好" in the text that needs to be filled in.
+
+If the number of teachers of the course is more than 3, the program will evaluate the course first and then the teacher.
+
+**Note:** If you encounter the problem that some teachers are skipped, it may be caused by the network condition. It is recommended to run the programme again.
+
+## Links
+
+* [LZU Auto Course Evaluation Feedback Script](LZU-Auto-Course-Evaluation-Feedback.py)
+
+Tested on 2021-11-12, 2021/22 Mid-term Course Evaluation of the First Semester.
+
+**Warning**:
+
+***For TESTING ONLY, not for any ILLIGAL USE!***
+
+***I will not be responsible for any adverse consequences caused by using this code.***
